ee1d317292ea2ec1d9dbb01859e4c07ffcc53c44
Heitor Sammuel Carvalho Souza <hscs@cin.ufpe.br>
2018-02-21 00:30:18 -0300

    possible workaround for vue problem

--- app/assets/javascripts/dynamicCover.js
+++ app/assets/javascripts/dynamicCover.js
@@ -1,7 +1,14 @@
+
+    var value = "";
+    value = $('#mymovieshistory_cover_image').val();
+    console.log("logging: ");
+    console.log(value);
+
+
 var coverImg = new Vue({
     el: '#movie-cover-sync',
     data: {
-        coverLink: ""
+        coverLink: value
     }
 });
 

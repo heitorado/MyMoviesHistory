77c6224ef5cd8445592be6805ce21098d2193d3f
Heitor Sammuel Carvalho Souza <hscs@cin.ufpe.br>
2018-02-20 22:55:02 -0300

    add vue integration on application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+// require rails-ujs
+// require turbolinks
 //= require_tree .
+//= require vue
+//= require vue-router
+//= require vue-resource
+//= require vuex

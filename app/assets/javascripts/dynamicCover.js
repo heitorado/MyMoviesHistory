448ad7f170af6aa3d20f0b2766d063c71f05445b
Heitor Sammuel Carvalho Souza <hscs@cin.ufpe.br>
2018-02-20 22:56:21 -0300

    implement vue.js reactive design on form image

--- app/assets/javascripts/dynamicCover.js
+++ app/assets/javascripts/dynamicCover.js
@@ -1,7 +1,8 @@
-new Vue({
-    el: '#movie-cover',
+var coverImg = new Vue({
+    el: '#movie-cover-sync',
     data: {
-        coverLink: 'http://www.pixedelic.com/themes/geode/demo/wp-content/uploads/sites/4/2014/04/placeholder4.png'
+        coverLink: ""
     }
 });
+
 
